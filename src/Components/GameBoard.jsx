530d4c009d4030c391015b9028c812302a266910
Aryan Raj <aryanraj24032002@gmail.com>
2025-01-03 16:00:11 +0530

    Game Board V1

---
+++ src/Components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const intialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameboard, setGameBoard] = useState(intialGameBoard);
+
+  function handleSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+  return (
+    <ol id="game-board">
+      {gameboard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
